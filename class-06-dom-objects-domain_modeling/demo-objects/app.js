0eef7e6aa2faebbe363d75adfd0ab660a47960e1
Sam Hamm <samhamm@gmail.com>
2016-07-25 10:50:05 -0700

    middle of class 7/25

---
+++ class-06-dom-objects-domain_modeling/demo-objects/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var myArray = ['a', 'b', 'c'];
+console.log('myArray is', myArray);
+
+var myObject = {
+  0: 'a',
+  1: 'b',
+  2: 'c'
+};
+console.log('myObject is', myObject);
+
+var emptyObject = {};
+var oneLineObject = { a: 1, b: 2 };
+
+var genericObject = {
+  key1: 'value1',
+  key2: 'value2',
+  'multi-word key': 'value3',
+  method: function() {
+    //do stuff
+  }
+};
+
+var sam = {
+  //properties
+  firstName: 'Sam',
+  middleName: null,
+  lastName: 'Hamm',
+  rating: 0,
+  isABoss: true,
+  underlings: ['Aaron', 'Munir', 'Annaka'],
+
+  //methods
+  getRating: function() {
+    return this.rating;
+  },
+  setRating: function(num) {
+    return this.rating = num;
+  }
+ };
+
+sam.employer = {
+  name: 'Code Fellows',
+  location: 'Seattle'
+};
+
+sam.logName = function() {
+  console.log(this.firstName + ' ' + this.lastName);
+};
+
+sam.whatIsThis = function() {
+  console.log(this);
+  console.dir(this);
+};
+
+sam.whatIsThis(); //logs the sam object
