1926251e5ea091005e42fe3812bbe90a9ac35d0e
Sam Hamm <samhamm@gmail.com>
2016-07-26 10:59:16 -0700

    middle of class 7/26

--- class-07-oop-constructors-tables/demo-constructors/app.js
+++ class-07-oop-constructors-tables/demo-constructors/app.js
@@ -1,16 +1,16 @@
 // Demo for objects-to-constructors
 'use strict';
 
-var britt = {
-  course: '201d11',
-  firstName: 'Britt',
-  lastInitial: 'J',
-  faveNumber: 16,
-  isCodeNinja: true,
-  introduction: function() {
-    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
-  }
-};
+// var britt = {
+//   course: '201d11',
+//   firstName: 'Britt',
+//   lastInitial: 'J',
+//   faveNumber: 13,
+//   isCodeNinja: true,
+//   introduction: function() {
+//     console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+//   }
+// };
 
 // I would need 154 lines of code to model our class this way
 // (14 students * 11 lines each)
@@ -18,3 +18,38 @@ var britt = {
 // What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
 
 var mahClass = [];
+
+// var britt = {
+function Student(firstName, lastInitial, faveNumber) {
+  this.course = '201d11';
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.faveNumber = faveNumber;
+  this.isCodeNinja = true;
+  this.introduction = function() {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  };
+  mahClass.push(this);
+  console.log('An object was made for', this.firstName);
+};
+
+var britt = new Student('Britt', 'J', 13); //eslint-disable-line
+var lee = new Student('Lee', 'B', 42); //eslint-disable-line
+var michael = new Student('Michael', 'M', 48); //eslint-disable-line
+var sera = new Student('Sera', 'S', 37); //eslint-disable-line
+
+function listStudents() {
+  // get a grip on the <ol> in the HTML
+  var studentList = document.getElementById('students');
+  // iterating through the array of students requires a for loop
+  for (var index = 0; index < mahClass.length; index++) {
+    // create a list element
+    var listElement = document.createElement('li');
+    // give the list element text content of a student's first name
+    listElement.textContent = mahClass[index].firstName + ' loves the number ' + mahClass[index].faveNumber;
+    // append the list element to the ol
+    studentList.appendChild(listElement);
+  }
+}
+
+listStudents();
