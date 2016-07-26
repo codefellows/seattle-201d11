78b6b2863e1a54ffb52b48b81482e1ae9a57520e
Sam Hamm <samhamm@gmail.com>
2016-07-26 12:29:09 -0700

    after class 7/26

--- class-07-oop-constructors-tables/demo-tables/app.js
+++ class-07-oop-constructors-tables/demo-tables/app.js
@@ -47,5 +47,104 @@ We'll need to create and append HTML elements in the same order.
 
 We have already put an empty <table> element in the HTML, with id="cats" */
 
+
+
 var catTable = document.getElementById('catjs');
 console.log('catTable is', catTable);
+
+function makeHeaderRow() {
+  // <tr>              create tr
+  var tableRow = document.createElement('tr');
+
+  //   <th>Name</th>   create a th, give it content, add it to tr
+  var thElement = document.createElement('th');
+  thElement.textContent = 'Name';
+  tableRow.appendChild(thElement);
+
+  //   <th>Color</th>  create a th, give it content, add it to tr
+  thElement = document.createElement('th');
+  thElement.textContent = 'Color';
+  tableRow.appendChild(thElement);
+
+  //   <th>Tail</th>   create a th, give it content, add it to tr
+  thElement = document.createElement('th');
+  thElement.textContent = 'Tail';
+  tableRow.appendChild(thElement);
+
+  // </tr>             add tr to the table
+  catTable.appendChild(tableRow);
+}
+
+function makeBuddyRow() {
+  // <tr>              create tr
+  var tableRow = document.createElement('tr');
+
+  //   <th>Name</th>   create a th, give it content, add it to tr
+  var tdElement = document.createElement('td');
+  tdElement.textContent = allCats[0].name;
+  tableRow.appendChild(tdElement);
+
+  //   <td>Color</td>  create a td, give it content, add it to tr
+  tdElement = document.createElement('td');
+  tdElement.textContent = allCats[0].color;
+  tableRow.appendChild(tdElement);
+
+  //   <td>Tail</td>   create a td, give it content, add it to tr
+  tdElement = document.createElement('td');
+  tdElement.textContent = allCats[0].tail;
+  tableRow.appendChild(tdElement);
+
+  // </tr>             add tr to the table
+  catTable.appendChild(tableRow);
+}
+
+function makeAlistairRow() {
+  // <tr>              create tr
+  var tableRow = document.createElement('tr');
+
+  //   <th>Name</th>   create a th, give it content, add it to tr
+  var tdElement = document.createElement('td');
+  tdElement.textContent = allCats[1].name;
+  tableRow.appendChild(tdElement);
+
+  //   <td>Color</td>  create a td, give it content, add it to tr
+  tdElement = document.createElement('td');
+  tdElement.textContent = allCats[1].color;
+  tableRow.appendChild(tdElement);
+
+  //   <td>Tail</td>   create a td, give it content, add it to tr
+  tdElement = document.createElement('td');
+  tdElement.textContent = allCats[1].tail;
+  tableRow.appendChild(tdElement);
+
+  // </tr>             add tr to the table
+  catTable.appendChild(tableRow);
+}
+
+function makeAllCatRows() {
+
+  for (var cat = 0; cat < allCats.length; cat++) {
+    // <tr>              create tr
+    var tableRow = document.createElement('tr');
+
+    //   <th>Name</th>   create a th, give it content, add it to tr
+    var tdElement = document.createElement('td');
+    tdElement.textContent = allCats[cat].name;
+    tableRow.appendChild(tdElement);
+
+    //   <td>Color</td>  create a td, give it content, add it to tr
+    tdElement = document.createElement('td');
+    tdElement.textContent = allCats[cat].color;
+    tableRow.appendChild(tdElement);
+
+    //   <td>Tail</td>   create a td, give it content, add it to tr
+    tdElement = document.createElement('td');
+    tdElement.textContent = allCats[cat].tail;
+    tableRow.appendChild(tdElement);
+
+    // </tr>             add tr to the table
+  }
+  catTable.appendChild(tableRow);
+}
+
+makeHeaderRow();
